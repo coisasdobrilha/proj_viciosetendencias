db93a7278beeddf9f5145dc231f6cbba9bbd3df9
Coisas do Brilha <coisasdobrilha@gmail.com>
2017-02-07 19:20:43 +0000

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5
+                    Laravel 55
                 </div>
 
                 <div class="links">
